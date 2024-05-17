a51f246ab8d546f05e23248bbcbdeafc84838e9f
sreesaradh97 <sreesaradh@crantia.com>
2024-05-17 19:21:50 +0530

    project setup

---
+++ resources/views/layouts/admin/common/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2023-2024
+        <a href="#" target="_blank">srd</a>.</strong>
+    All rights reserved.
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/jquery.validate.min.js')}}"></script>
+<script src="{{asset('js/admin/adminlte.min.js')}}"></script>
+<script src="https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js"></script>
+<script src="{{asset('js/jquery.validate.min.js')}}"></script>
+<script src="{{asset('js/admin/validator.js')}}"></script>
+@stack("scripts")
